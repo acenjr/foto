5d940ba7314debd3a6fda8c608c8f691b59e523c
Kelvin Njeru <njrkelvin@gmail.com>
2025-01-01 18:00:32 +0300

    fixed utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
+import { aspectRatioOptions } from "../../constants";
 
 // import { aspectRatioOptions } from "@/constants";
 
