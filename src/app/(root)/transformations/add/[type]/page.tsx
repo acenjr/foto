0a15dcfb264a79de4fa1e4c72814311a9452491e
Kelvin Njeru <njrkelvin@gmail.com>
2025-01-02 19:36:39 +0300

    added to UI

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,55 +1,33 @@
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import Header from "@/components/shared/Header";
 import TransformationForm from "@/components/shared/TransformationForm";
-import { getUserById } from "@/lib/actions/user.actions";
 import { transformationTypes } from "../../../../../../constants";
-
-interface SearchParamProps {
-  params: {
-    type: string;
-  };
-}
-
-interface TransformationType {
-  title: string;
-  subTitle: string;
-  type: TransformationTypeKey;
-}
-
-type TransformationTypeKey = keyof typeof transformationTypes;
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const AddTransformationTypePage = async ({
   params: { type },
 }: SearchParamProps) => {
-  const { userId } = auth();
+  // const { userId } = auth();
+  const transformation = transformationTypes[type];
 
-  if (!userId) {
-    redirect("/sign-in");
-  }
+  // if (!userId) redirect("/sign-in");
 
-  const transformation = transformationTypes[type as TransformationTypeKey];
-
-  // if (!transformation) {
-  //   redirect("/"); // Redirect to home if invalid transformation type
-  // }
-
-  const user = await getUserById(userId);
-
-  // if (!user) {
-  //   redirect("/sign-in"); // Redirect if user not found
-  // }
+  // const user = await getUserById(userId);
 
   return (
-    <div className="flex flex-col gap-4">
+    <>
       <Header title={transformation.title} subTitle={transformation.subTitle} />
-      <TransformationForm
-        action="Add"
-        userId={user._id}
-        type={transformation.type as TransformationTypeKey}
-        creditBalance={user.creditBalance}
-      />
-    </div>
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={0}
+          userId={""}
+        />
+      </section>
+    </>
   );
 };
 
--- src/components/shared/TransformationForm.tsx
+++ src/components/shared/TransformationForm.tsx
@@ -14,9 +14,25 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { defaultValues } from "../../../constants";
+import {
+  aspectRatioOptions,
+  defaultValues,
+  transformationTypes,
+} from "../../../constants";
+import { CustomField } from "./CustomField";
 
-const formSchema = z.object({
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+import { AspectRatioKey } from "@/lib/utils";
+import { Istok_Web } from "next/font/google";
+
+export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
   color: z.string().optional(),
@@ -27,7 +43,19 @@ const formSchema = z.object({
 const TransformationForm = ({
   action,
   data = null,
+  userId,
+  type,
+  creditBalance,
+  config = null,
 }: TransformationFormProps) => {
+  const transformationType = transformationTypes[type];
+  const [image, setImage] = useState(data);
+  const [newTransformation, setNewTransformation] =
+    useState<Transformations | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isTransforming, setIsTransforming] = useState(false);
+  const [transformationConfig, setTransformationConfig] = useState(config);
+
   const initialValues =
     data && action === "Update"
       ? {
@@ -51,26 +79,123 @@ const TransformationForm = ({
     console.log(values);
   }
 
+  const onSelectFileHandler = (
+    value: string,
+    onChangeField: (value: string) => void
+  ) => {};
+
+  const onInputChangeHandler = (
+    fieldName: string,
+    value: string,
+    type: string,
+    onChangeField: (value: string) => void
+  ) => {};
+
+  const onTransFormHandler = () => {};
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+        <CustomField
           control={form.control}
           name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          formLabel="Image Title"
+          className="w-full "
+          render={({ field }) => <Input {...field} className="input-field" />}
         />
-        <Button type="submit">Submit</Button>
+        {type === "fill" && (
+          <CustomField
+            control={form.control}
+            name="aspectRatio"
+            formLabel="Aspect Ratio"
+            className="w-full"
+            render={({ field }) => (
+              <Select
+                onValueChange={(value) =>
+                  onSelectFileHandler(value, field.onChange)
+                }
+              >
+                <SelectTrigger className="select-field">
+                  <SelectValue placeholder="Select Size" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Object.keys(aspectRatioOptions).map((key) => (
+                    <SelectItem key={key} value={key} className="select-item">
+                      {aspectRatioOptions[key as AspectRatioKey].label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          />
+        )}
+
+        {(type === "remove" || type === "recolor") && (
+          <div className="prompt-field">
+            <CustomField
+              control={form.control}
+              name="prompt"
+              formLabel={
+                type === "remove" ? "Object to remove" : "Object to recolor"
+              }
+              className="w-full"
+              render={({ field }) => (
+                <Input
+                  value={field.value}
+                  className="input-field"
+                  onChange={(e) =>
+                    onInputChangeHandler(
+                      "prompt",
+                      e.target.value,
+                      type,
+                      field.onChange
+                    )
+                  }
+                />
+              )}
+            />
+          </div>
+        )}
+
+        {type === "recolor" && (
+          <CustomField
+            control={form.control}
+            name="color"
+            formLabel="Replacement color"
+            className="w-full"
+            render={({ field }) => (
+              <Input
+                value={field.value}
+                className="input-field"
+                onChange={(e) =>
+                  onInputChangeHandler(
+                    "color",
+                    e.target.value,
+                    "recolor",
+                    field.onChange
+                  )
+                }
+              />
+            )}
+          />
+        )}
+        <div className="flex flex-col gap-4">
+          <Button
+            type="button"
+            className="submit-button capitalize"
+            disabled={isTransforming || newTransformation === null}
+            onClick={onTransFormHandler}
+          >
+            {isTransforming ? "Transforming..." : "Apply Transformation"}
+          </Button>
+          <Button
+            type="submit"
+            className="submit-button capitalize"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting..." : "Save Image"}
+          </Button>
+        </div>
       </form>
     </Form>
   );
